fc56ba103c7324f779f57c54f647c542e6cdd320
lucasfturos <lucasfturos@gmail.com>
2024-06-02 18:31:15 -0300

    Esqueci xD

--- src/main.c
+++ src/main.c
@@ -3,35 +3,6 @@
 #include "SDL/render.h"
 #include <time.h>
 
-void UNUSED run() {
-    srand(time(0));
-    Resources res;
-    initWindow(&res);
-    int quit = 0;
-    while (!quit) {
-        SDL_Event event;
-        while (SDL_PollEvent(&event) != 0) {
-            event.type == SDL_QUIT ? (quit = 1) : 0;
-            switch (event.key.keysym.sym) {
-            case SDLK_ESCAPE:
-                quit = 1;
-                break;
-            default:
-                break;
-            }
-        }
-        SDL_SetRenderDrawColor(res.ren, 0x00, 0x00, 0x00, 0xFF);
-        SDL_RenderClear(res.ren);
-
-        render1(res.ren);
-
-        SDL_RenderPresent(res.ren);
-        SDL_Delay(1000 / FPS);
-    }
-    destroyResources(res);
-    SDL_Quit();
-}
-
 int main(void) {
     srand(time(0));
     Resources res;
